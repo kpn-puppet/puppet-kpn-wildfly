e6d4c022d0eb7aa7215a65ce51f5e8aff10d3746
Cees-Jan Swart <cj.swart@quicknet.nl>
2019-02-12 09:38:54 +0100

    fix interface settings

--- manifests/config/interface.pp
+++ manifests/config/interface.pp
@@ -1,8 +1,8 @@
 # set the interfaces
 define wildfly::config::interface (
-  Stdlib::Unixpath                                                $catalina_home,
-  Stdlib::Compat::Ip_address                                      $ip_address,
-  Enum['management-http','management-https','ajp','http','https'] $ip_type,
+  Stdlib::Unixpath               $catalina_home,
+  Stdlib::Compat::Ip_address     $ip_address,
+  Enum['management','public']    $interface,
 ) {
 
   tag(sha1($catalina_home))
@@ -11,11 +11,11 @@ define wildfly::config::interface (
     fail('Server configurations require Augeas >= 1.0.0')
   }
 
-  augeas { "${catalina_home} ${ip_address} ${ip_type}":
+  augeas { "${catalina_home} ${ip_address} ${interface}":
     context => "/files/${catalina_home}/standalone/configuration/standalone.xml",
     lens    => 'Xml.lns',
     incl    => "${catalina_home}/standalone/configuration/standalone.xml",
-    changes => "set server/interfaces/interface[#attribute/name= \"${ip_type}\"]/inet-address/#attribute/value ${ip_address}",
+    changes => "set server/interfaces/interface[#attribute/name= \"${interface}\"]/inet-address/#attribute/value ${ip_address}",
   }
 }
 
